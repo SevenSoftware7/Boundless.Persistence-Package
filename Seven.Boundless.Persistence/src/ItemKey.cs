faab89840b035a864141a5d2708ef25785056ac9
AxelSeven <spaghettiandmeatballs61@gmail.com>
2025-09-20 17:25:10 +0200

    refactor: Change ItemKey from struct to record struct

--- Seven.Boundless.Persistence/src/ItemKey.cs
+++ Seven.Boundless.Persistence/src/ItemKey.cs
@@ -3,7 +3,7 @@ namespace Seven.Boundless.Persistence;
 using System;
 
 [Serializable]
-public partial struct ItemKey : IEquatable<ItemKey> {
+public readonly partial record struct ItemKey {
 	[System.Text.RegularExpressions.GeneratedRegex(@"[^a-z0-9\-_=\|!#~]")]
 	private static partial System.Text.RegularExpressions.Regex ItemKeySanitizeRegex();
 
@@ -14,19 +14,12 @@ public partial struct ItemKey : IEquatable<ItemKey> {
 	public readonly string String;
 
 	public ItemKey(string @string) {
-		if (string.IsNullOrWhiteSpace(@string)) throw new ArgumentException("Key cannot be null or empty.", nameof(@string));
-		String = ToItemKeyFormat(@string);
+		String = !string.IsNullOrWhiteSpace(@string)
+			? ToItemKeyFormat(@string)
+			: throw new ArgumentException("Key cannot be null or empty.", nameof(@string));
 	}
 
 	public override string ToString() => String;
-	public override int GetHashCode() => String.GetHashCode();
-	public override bool Equals(object? obj) => obj is ItemKey key && String == key.String;
-
-	public bool Equals(ItemKey other) => String == other.String;
-
-
-	public static bool operator ==(ItemKey? left, ItemKey? right) => left?.Equals(right) ?? right is null;
-	public static bool operator !=(ItemKey? left, ItemKey? right) => !(left == right);
 
 	public static implicit operator string(ItemKey key) => key.String;
 }
